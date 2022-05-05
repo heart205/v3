016882feb2491ec20b96b605c6762c15469c1754
heart <7362469@qq.com>
2022-05-05 14:21:09 +0800

    build: init

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,30 +1,10 @@
-import { defineAsyncComponent } from 'vue'
-
 import * as VueRouter from 'vue-router'
+import { defineAsyncComponent } from 'vue'
 export const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: defineAsyncComponent(() => import('../pages/home/index.vue'))
-  },
-  {
-    path: '/custom-canvas',
-    name: 'CustomCanvas',
-    component: defineAsyncComponent(
-      () => import('../pages/canvas/customCanvas/index.vue')
-    )
-  },
-  {
-    path: '/canvas-waterMark',
-    name: 'CanvasWaterMark',
-    component: defineAsyncComponent(
-      () => import('../pages/canvas/waterMark/index.vue')
-    )
-  },
-  {
-    path: '/mouse/mouse-down-hook',
-    name: 'MouseDownHook',
-    component: defineAsyncComponent(() => import('../pages/mouse/down.vue'))
+    path: '/:pathMatch(.*)',
+    name: '404',
+    component: defineAsyncComponent(() => import('../pages/404/index.vue'))
   }
 ]
 
